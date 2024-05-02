14f4a013045ed44df5fea8d02fc63d36d6f9dea2
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-02 16:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+const btn=document.getElementById('swap')
+const app=document.getElementById('app')
+
+btn.addEventListener('click',()=>{
+	if(app.className=="day"){
+
+		app.className="nigth"
+	}
+	else{
+		app.className='day'
+	}
+})
 
